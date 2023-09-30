0be955174a189ca4bf8a49dd6154f0b53cab479b
kg4vdk <mike@kg4vdk.com>
2023-09-30 18:03:07 -0500

    Fuck it, let's do it live

--- station_build.sh
+++ station_build.sh
@@ -4,6 +4,19 @@ TEST=0
 # Build directory
 BUILD_DIR=$(pwd)
 
+##############################################
+# Amateur radio software versions to install #
+##############################################
+HAMLIB_VER=4.5.5 # from source
+FLDIGI_VER=4.2.00 # source
+FLRIG_VER=2.0.03 # source
+FLMSG_VER=4.0.23 # source
+FLWRAP_VER=1.3.6 # source
+FLAMP_VER=2.2.09 # source
+JS8CALL_VER=2.2.0 # appimage
+WSJTX_VER=2.6.1 # deb
+HAMRS_VER=1.0.6 # appimage
+
 # Disclaimers and warnings
 echo "##################################################"
 echo -e "
@@ -45,9 +58,9 @@ fi
 ###############################
 # Make sure system is updated #
 ###############################
-#sudo cp "${BUILD_DIR}/apt/official-source-repositories.list" /etc/apt/sources.list.d/official-source-repositories.list
-#sudo apt update
-#sudo apt upgrade --yes
+sudo cp "${BUILD_DIR}/apt/official-source-repositories.list" /etc/apt/sources.list.d/official-source-repositories.list
+sudo apt update
+sudo apt upgrade --yes
 
 ######################################
 # Install Virtualbox Guest Additions #
@@ -72,153 +85,140 @@ sudo rmdir /tmp/VBOX_GA_ISO
 #############################
 # Add user to dialout group #
 #############################
-#sudo usermod --append --groups dialout $USER
+sudo usermod --append --groups dialout $USER
 
 ########################
 # Required directories #
 ########################
-#sudo mkdir --verbose /appimage
+sudo mkdir --verbose /appimage
 
 #####################
 # GPS and GPS clock #
 #####################
-#sudo apt install --yes gpsd gpsd-clients chrony
-#sudo cp --verbose "${BUILD_DIR}/config/gpsd" /etc/default/gpsd
-#sudo cp --verbose "${BUILD_DIR}/config/chrony.conf" /etc/chrony/chrony.conf
+sudo apt install --yes gpsd gpsd-clients chrony
+sudo cp --verbose "${BUILD_DIR}/config/gpsd" /etc/default/gpsd
+sudo cp --verbose "${BUILD_DIR}/config/chrony.conf" /etc/chrony/chrony.conf
 
 ##############
 # Gridsquare #
 ##############
-#sudo apt install --yes ruby
-#sudo gem install gpsd_client maidenhead
-#cp --verbose "${BUILD_DIR}/bin/gridsquare.rb" /usr/bin/gridsquare.rb
+sudo apt install --yes ruby
+sudo gem install gpsd_client maidenhead
+cp --verbose "${BUILD_DIR}/bin/gridsquare.rb" /usr/bin/gridsquare.rb
 
 ##########################
 # Crontab for gridsquare #
 ##########################
-#(crontab -l; cat "${BUILD_DIR}/config/crontab") | crontab -
-
-##############################################
-# Amateur radio software versions to install #
-##############################################
-HAMLIB_VER=4.5.5 # from source
-FLDIGI_VER=4.2.00 # source
-FLRIG_VER=2.0.03 # source
-FLMSG_VER=4.0.23 # source
-FLWRAP_VER=1.3.6 # source
-FLAMP_VER=2.2.09 # source
-JS8CALL_VER=2.2.0 # appimage
-WSJTX_VER=2.6.1 # deb
-HAMRS_VER=1.0.6 # appimage
+(crontab -l; cat "${BUILD_DIR}/config/crontab") | crontab -
 
 ##################
 # Install Hamlib #
 ##################
-#HAMLIB_DIR="${BUILD_DIR}/hamlib"
-#HAMLIB_URL_BASE="https://github.com/Hamlib/Hamlib/releases/download"
+HAMLIB_DIR="${BUILD_DIR}/hamlib"
+HAMLIB_URL_BASE="https://github.com/Hamlib/Hamlib/releases/download"
 
-#mkdir --parents --verbose "${HAMLIB_DIR}"
-#cd "${HAMLIB_DIR}"
-#wget "${HAMLIB_URL_BASE}/${HAMLIB_VER}/hamlib-${HAMLIB_VER}.tar.gz"
-#tar -xvzf "${HAMLIB_DIR}/hamlib-${HAMLIB_VER}.tar.gz"
+mkdir --parents --verbose "${HAMLIB_DIR}"
+cd "${HAMLIB_DIR}"
+wget "${HAMLIB_URL_BASE}/${HAMLIB_VER}/hamlib-${HAMLIB_VER}.tar.gz"
+tar -xvzf "${HAMLIB_DIR}/hamlib-${HAMLIB_VER}.tar.gz"
 
-#cd "hamlib-${HAMLIB_VER}"
-#./configure
-#make
-#sudo make install
+cd "hamlib-${HAMLIB_VER}"
+./configure
+make
+sudo make install
 
 #######################################
 # Install FL Suite (and dependencies) #
 #######################################
-#FL_DIR="${BUILD_DIR}/fl_suite"
-#FL_URL_BASE="http://www.w1hkj.com/files"
+FL_DIR="${BUILD_DIR}/fl_suite"
+FL_URL_BASE="http://www.w1hkj.com/files"
 
-#mkdir --parents --verbose "${FL_DIR}"
-#cd "${FL_DIR}"
+mkdir --parents --verbose "${FL_DIR}"
+cd "${FL_DIR}"
 
-#wget "${FL_URL_BASE}/fldigi/fldigi-${FLDIGI_VER}.tar.gz"
-#wget "${FL_URL_BASE}/flrig/flrig-${FLRIG_VER}.tar.gz"
-#wget "${FL_URL_BASE}/flmsg/flmsg-${FLMSG_VER}.tar.gz"
-#wget "${FL_URL_BASE}/flwrap/flwrap-${FLWRAP_VER}.tar.gz"
-#wget "${FL_URL_BASE}/flamp/flamp-${FLAMP_VER}.tar.gz"
+wget "${FL_URL_BASE}/fldigi/fldigi-${FLDIGI_VER}.tar.gz"
+wget "${FL_URL_BASE}/flrig/flrig-${FLRIG_VER}.tar.gz"
+wget "${FL_URL_BASE}/flmsg/flmsg-${FLMSG_VER}.tar.gz"
+wget "${FL_URL_BASE}/flwrap/flwrap-${FLWRAP_VER}.tar.gz"
+wget "${FL_URL_BASE}/flamp/flamp-${FLAMP_VER}.tar.gz"
 
-#tar -xvzf "${FL_DIR}/fldigi-${FLDIGI_VER}.tar.gz"
-#tar -xvzf "${FL_DIR}/flrig-${FLRIG_VER}.tar.gz"
-#tar -xvzf "${FL_DIR}/flmsg-${FLMSG_VER}.tar.gz"
-#tar -xvzf "${FL_DIR}/flwrap-${FLWRAP_VER}.tar.gz"
-#tar -xvzf "${FL_DIR}/flamp-${FLAMP_VER}.tar.gz"
+tar -xvzf "${FL_DIR}/fldigi-${FLDIGI_VER}.tar.gz"
+tar -xvzf "${FL_DIR}/flrig-${FLRIG_VER}.tar.gz"
+tar -xvzf "${FL_DIR}/flmsg-${FLMSG_VER}.tar.gz"
+tar -xvzf "${FL_DIR}/flwrap-${FLWRAP_VER}.tar.gz"
+tar -xvzf "${FL_DIR}/flamp-${FLAMP_VER}.tar.gz"
 
-#sudo apt build-dep --yes fldigi
+sudo apt build-dep --yes fldigi
 
-#cd "${FL_DIR}/fldigi-${FLDIGI_VER}"
-#./configure
-#make
-#sudo make install
+cd "${FL_DIR}/fldigi-${FLDIGI_VER}"
+./configure
+make
+sudo make install
 
-#cd "${FL_DIR}/flrig-${FLRIG_VER}"
-#./configure
-#make
-#sudo make install
+cd "${FL_DIR}/flrig-${FLRIG_VER}"
+./configure
+make
+sudo make install
 
-#cd "${FL_DIR}/flmsg-${FLMSG_VER}"
-#./configure
-#make
-#sudo make install
+cd "${FL_DIR}/flmsg-${FLMSG_VER}"
+./configure
+make
+sudo make install
 
-#cd "${FL_DIR}/flwrap-${FLWRAP_VER}"
-#./configure
-#make
-#sudo make install
+cd "${FL_DIR}/flwrap-${FLWRAP_VER}"
+./configure
+make
+sudo make install
 
-#cd "${FL_DIR}/flamp-${FLAMP_VER}"
-#./configure
-#make
-#sudo make install
+cd "${FL_DIR}/flamp-${FLAMP_VER}"
+./configure
+make
+sudo make install
 
 ###################
 # Install JS8Call #
 ###################
-#JS8CALL_DIR="${BUILD_DIR}/js8call"
-#JS8CALL_URL_BASE="http://files.js8call.com"
+JS8CALL_DIR="${BUILD_DIR}/js8call"
+JS8CALL_URL_BASE="http://files.js8call.com"
 
-#mkdir --parents --verbose "${JS8CALL_DIR}"
-#cd "${JS8CALL_DIR}"
+mkdir --parents --verbose "${JS8CALL_DIR}"
+cd "${JS8CALL_DIR}"
 
-#wget "${JS8CALL_URL_BASE}/${JS8CALL_VER}/js8call-${JS8CALL_VER}-Linux-Desktop.x86_64.AppImage"
-#chmod +x "${JS8CALL_DIR}/js8call-${JS8CALL_VER}-Linux-Desktop.x86_64.AppImage"
-#sudo cp --verbose "js8call-${JS8CALL_VER}-Linux-Desktop.x86_64.AppImage" /appimage
-#sudo cp --verbose "${BUILD_DIR}/desktop/JS8Call.desktop" /usr/share/applications
-#sudo cp --verbose "${BUILD_DIR}/icons/js8call.png" /usr/share/icons
+wget "${JS8CALL_URL_BASE}/${JS8CALL_VER}/js8call-${JS8CALL_VER}-Linux-Desktop.x86_64.AppImage"
+chmod +x "${JS8CALL_DIR}/js8call-${JS8CALL_VER}-Linux-Desktop.x86_64.AppImage"
+sudo cp --verbose "js8call-${JS8CALL_VER}-Linux-Desktop.x86_64.AppImage" /appimage
+sudo cp --verbose "${BUILD_DIR}/desktop/JS8Call.desktop" /usr/share/applications
+sudo cp --verbose "${BUILD_DIR}/icons/js8call.png" /usr/share/icons
 
 ##################
 # Install WSJT-X #
 ##################
-#WSJTX_DIR="${BUILD_DIR}/wsjtx"
-#WSJTX_URL_BASE="https://wsjt.sourceforge.io/downloads"
+WSJTX_DIR="${BUILD_DIR}/wsjtx"
+WSJTX_URL_BASE="https://wsjt.sourceforge.io/downloads"
 
-#mkdir --parents --verbose "${WSJTX_DIR}"
-#cd "${WSJTX_DIR}"
+mkdir --parents --verbose "${WSJTX_DIR}"
+cd "${WSJTX_DIR}"
 
-#sudo apt build-dep --yes wsjtx
-#sudo apt install --yes libqt5multimedia5-plugins
+sudo apt build-dep --yes wsjtx
+sudo apt install --yes libqt5multimedia5-plugins
 
-#wget "${WSJTX_URL_BASE}/wsjtx_${WSJTX_VER}_amd64.deb"
-#sudo dpkg -i "${WSJTX_DIR}/wsjtx_${WSJTX_VER}_amd64.deb"
+wget "${WSJTX_URL_BASE}/wsjtx_${WSJTX_VER}_amd64.deb"
+sudo dpkg -i "${WSJTX_DIR}/wsjtx_${WSJTX_VER}_amd64.deb"
 
 #################
 # Install HamRS #
 #################
-#HAMRS_DIR="${BUILD_DIR}/hamrs"
-#HAMRS_URL_BASE="https://hamrs-releases.s3.us-east-2.amazonaws.com"
+HAMRS_DIR="${BUILD_DIR}/hamrs"
+HAMRS_URL_BASE="https://hamrs-releases.s3.us-east-2.amazonaws.com"
 
-#mkdir --parents --verbose "${HAMRS_DIR}"
-#cd "${HAMRS_DIR}"
+mkdir --parents --verbose "${HAMRS_DIR}"
+cd "${HAMRS_DIR}"
 
-#wget "${HAMRS_URL_BASE}/${HAMRS_VER}/hamrs-${HAMRS_VER}-linux-x86_64.AppImage"
-#chmod +x "${HAMRS_DIR}/hamrs-${HAMRS_VER}-linux-x86_64.AppImage"
-#sudo cp --verbose "hamrs-${HAMRS_VER}-linux-x86_64.AppImage" /appimage
-#sudo cp --verbose "${BUILD_DIR}/desktop/HamRS.desktop" /usr/share/applications
-#sudo cp --verbose "${BUILD_DIR}/icons/hamrs.png" /usr/share/icons
+wget "${HAMRS_URL_BASE}/${HAMRS_VER}/hamrs-${HAMRS_VER}-linux-x86_64.AppImage"
+chmod +x "${HAMRS_DIR}/hamrs-${HAMRS_VER}-linux-x86_64.AppImage"
+sudo cp --verbose "hamrs-${HAMRS_VER}-linux-x86_64.AppImage" /appimage
+sudo cp --verbose "${BUILD_DIR}/desktop/HamRS.desktop" /usr/share/applications
+sudo cp --verbose "${BUILD_DIR}/icons/hamrs.png" /usr/share/icons
 
 ########################################
 # Add login screen background and logo #
@@ -247,7 +247,9 @@ echo "draw-user-backgrounds=false" | sudo tee --append /etc/lightdm/slick-greete
 gsettings set org.cinnamon.desktop.background picture-uri "file:///usr/share/backgrounds/bg.png"
 gsettings set org.cinnamon.desktop.background primary-color "#466480"
 
-# Reboot the system
+#####################
+# Reboot the system #
+#####################
 echo "Script completed, reboot required..."
 read -p "Reboot now? [Y/N]: " REBOOT
 if [ "${REBOOT}" == "Y" ] || [ "${REBOOT}" == "y" ]; then
