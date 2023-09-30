2f4e047f642b7ccb7b59284d708b255e45dbabe1
kg4vdk <mike@kg4vdk.com>
2023-09-30 16:41:33 -0500

    Trying to slim down

--- station_build.sh
+++ station_build.sh
@@ -33,61 +33,74 @@ else
 fi
 
 # Check if the user ACTUALLY read the files
-if [ "${TEST}" != 42 ]; then
-    echo "Try ACTUALLY reading them this time..."
-    echo "Exiting!"
-    exit 1
-fi
+#if [ "${TEST}" != 42 ]; then
+#    echo "Try ACTUALLY reading them this time..."
+#    echo "Exiting!"
+#    exit 1
+#fi
 
-cd "${BUILD_DIR}"
+############################################################
 
-# Make sure system is updated
+
+###############################
+# Make sure system is updated #
+###############################
+sudo cp "${BUILD_DIR}/apt/official-source-repositories.list" /etc/apt/sources.list.d/official-source-repositories.list
 sudo apt update
-sudo apt upgrade --yes
+#sudo apt upgrade --yes
 
-# Get Virtualbox version
-VBOX_VER=$(sudo dmidecode | grep -i vboxver | grep -E -o '[[:digit:]\.]+' | tail -n 1)
-VBOX_GA_VER=$(VBoxClient --version)
+######################################
+# Install Virtualbox Guest Additions #
+######################################
+#VBOX_VER=$(sudo dmidecode | grep -i vboxver | grep -E -o '[[:digit:]\.]+' | tail -n 1)
+#VBOX_GA_VER=$(VBoxClient --version)
 
-# Install Virtualbox Guest Additions
-VBOX_DIR="${BUILD_DIR}/${VBOX_DIR}"
-VBOX_URL_BASE="https://download.virtualbox.org/virtualbox"
+#VBOX_DIR="${BUILD_DIR}/vbox"
+#VBOX_URL_BASE="https://download.virtualbox.org/virtualbox"
 
-mkdir --parents --verbose "${VBOX_DIR}"
-cd "${VBOX_DIR}"
+#mkdir --parents --verbose "${VBOX_DIR}"
+#cd "${VBOX_DIR}"
 
-wget "${VBOX_URL_BASE}/${VBOX_VER}/VBoxGuestAdditions_${VBOX_VER}.iso"
+#wget "${VBOX_URL_BASE}/${VBOX_VER}/VBoxGuestAdditions_${VBOX_VER}.iso"
 
-mkdir /tmp/VBOX_GA_ISO
-sudo mount -t iso9660 -o loop "${VBOX_DIR}/VBoxGuestAdditions_${VBOX_VER}.iso" /tmp/VBOX_GA_ISO
-sudo sh /tmp/VBOX_GA_ISO/VBoxLinuxAdditions.run --nox11
-sudo umount /tmp/VBOX_GA_ISO
-sudo rmdir /tmp/VBOX_GA_ISO
+#mkdir /tmp/VBOX_GA_ISO
+#sudo mount -t iso9660 -o loop "${VBOX_DIR}/VBoxGuestAdditions_${VBOX_VER}.iso" /tmp/VBOX_GA_ISO
+#sudo sh /tmp/VBOX_GA_ISO/VBoxLinuxAdditions.run --nox11
+#sudo umount /tmp/VBOX_GA_ISO
+#sudo rmdir /tmp/VBOX_GA_ISO
 
-# Add user to dialout group
-sudo usermod --append --groups dialout $USER
+#############################
+# Add user to dialout group #
+#############################
+#sudo usermod --append --groups dialout $USER
 
-# Required directories
-echo "Creating needed directories..."
+########################
+# Required directories #
+########################
 sudo mkdir --verbose /appimage
 
-# GPS and GPS clock
-echo "Setting up GPSd and NMEA clock source..."
-sudo apt install --yes gpsd gpsd-clients chrony
-sudo cp --verbose "${BUILD_DIR}/config/gpsd" /etc/default/gpsd
-sudo cp --verbose "${BUILD_DIR}/config/chrony.conf" /etc/chrony/chrony.conf
+#####################
+# GPS and GPS clock #
+#####################
+#sudo apt install --yes gpsd gpsd-clients chrony
+#sudo cp --verbose "${BUILD_DIR}/config/gpsd" /etc/default/gpsd
+#sudo cp --verbose "${BUILD_DIR}/config/chrony.conf" /etc/chrony/chrony.conf
 
-# Gridsquare
-echo "Setting up gridsquare reporting..."
-sudo apt install --yes ruby
-sudo gem install gpsd_client maidenhead
-cp --verbose "${BUILD_DIR}/bin/gridsquare.rb" /usr/bin/gridsquare.rb
+##############
+# Gridsquare #
+##############
+#sudo apt install --yes ruby
+#sudo gem install gpsd_client maidenhead
+#cp --verbose "${BUILD_DIR}/bin/gridsquare.rb" /usr/bin/gridsquare.rb
 
-# Crontab for gridsquare
-(crontab -l; cat "${BUILD_DIR}/config/crontab") | crontab -
-crontab -l
+##########################
+# Crontab for gridsquare #
+##########################
+#(crontab -l; cat "${BUILD_DIR}/config/crontab") | crontab -
 
-# Amateur radio software versions to install
+##############################################
+# Amateur radio software versions to install #
+##############################################
 HAMLIB_VER=4.5.5 # from source
 FLDIGI_VER=4.2.00 # source
 FLRIG_VER=2.0.03 # source
@@ -98,7 +111,9 @@ JS8CALL_VER=2.2.0 # appimage
 WSJTX_VER=2.6.1 # deb
 HAMRS_VER=1.0.6 # appimage
 
-# Install Hamlib
+##################
+# Install Hamlib #
+##################
 HAMLIB_DIR="${BUILD_DIR}/hamlib"
 HAMLIB_URL_BASE="https://github.com/Hamlib/Hamlib/releases/download"
 
@@ -106,12 +121,15 @@ mkdir --parents --verbose "${HAMLIB_DIR}"
 cd "${HAMLIB_DIR}"
 wget "${HAMLIB_URL_BASE}/${HAMLIB_VER}/hamlib-${HAMLIB_VER}.tar.gz"
 tar -xvzf "${HAMLIB_DIR}/hamlib-${HAMLIB_VER}.tar.gz"
-cd "hamlib-${HAMLIB_VER}"
-./configure
-make
-sudo make install
 
-# Install FL Suite (and dependencies)
+cd "hamlib-${HAMLIB_VER}"
+#./configure
+#make
+#sudo make install
+
+#######################################
+# Install FL Suite (and dependencies) #
+#######################################
 FL_DIR="${BUILD_DIR}/fl_suite"
 FL_URL_BASE="http://www.w1hkj.com/files"
 
@@ -130,36 +148,36 @@ tar -xvzf "${FL_DIR}/flmsg-${FLMSG_VER}.tar.gz"
 tar -xvzf "${FL_DIR}/flwrap-${FLWRAP_VER}.tar.gz"
 tar -xvzf "${FL_DIR}/flamp-${FLAMP_VER}.tar.gz"
 
-sudo cp "${BUILD_DIR}/apt/official-source-repositories.list" /etc/apt/sources.list.d/official-source-repositories.list
-sudo apt update
-sudo apt build-dep --yes fldigi
+#sudo apt build-dep --yes fldigi
 
 cd "${FL_DIR}/fldigi-${FLDIGI_VER}"
-./configure
-make
-sudo make install
+#./configure
+#make
+#sudo make install
 
 cd "${FL_DIR}/flrig-${FLRIG_VER}"
-./configure
-make
-sudo make install
+#./configure
+#make
+#sudo make install
 
 cd "${FL_DIR}/flmsg-${FLMSG_VER}"
-./configure
-make
-sudo make install
+#./configure
+#make
+#sudo make install
 
 cd "${FL_DIR}/flwrap-${FLWRAP_VER}"
-./configure
-make
-sudo make install
+#./configure
+#make
+#sudo make install
 
 cd "${FL_DIR}/flamp-${FLAMP_VER}"
-./configure
-make
-sudo make install
+#./configure
+#make
+#sudo make install
 
-# Install JS8Call
+###################
+# Install JS8Call #
+###################
 JS8CALL_DIR="${BUILD_DIR}/js8call"
 JS8CALL_URL_BASE="http://files.js8call.com"
 
@@ -172,7 +190,9 @@ sudo cp --verbose "js8call-${JS8CALL_VER}-Linux-Desktop.x86_64.AppImage" /appima
 sudo cp --verbose "${BUILD_DIR}/desktop/JS8Call.desktop" /usr/share/applications
 sudo cp --verbose "${BUILD_DIR}/icons/js8call.png" /usr/share/icons
 
-# Install WSJT-X
+##################
+# Install WSJT-X #
+##################
 WSJTX_DIR="${BUILD_DIR}/wsjtx"
 WSJTX_URL_BASE="https://wsjt.sourceforge.io/downloads"
 
@@ -185,7 +205,9 @@ sudo apt install --yes libqt5multimedia5-plugins
 wget "${WSJTX_URL_BASE}/wsjtx_${WSJTX_VER}_amd64.deb"
 sudo dpkg -i "${WSJTX_DIR}/wsjtx_${WSJTX_VER}_amd64.deb"
 
-# Install HamRS
+#################
+# Install HamRS #
+#################
 HAMRS_DIR="${BUILD_DIR}/hamrs"
 HAMRS_URL_BASE="https://hamrs-releases.s3.us-east-2.amazonaws.com"
 
@@ -217,9 +239,9 @@ LOGO_TXT="KG4VDK" # If you want more or fewer characters, adjust the -size optio
 convert -background transparent -fill white -font ~/station_build/font/national-park.outline.otf -size 300x96 -pointsize 72 -gravity center "caption:${LOGO_TXT}" "${IMG_DIR}/logo.png"
 sudo cp --verbose "${IMG_DIR}/logo.png" /usr/share/backgrounds/logo.png
 
-echo "[Greeter]" | sudo tee /etc/lightdm/slick-greeter.conf
-echo "background=/usr/share/backgrounds/bg.png" | sudo tee --append /etc/lightdm/slick-greeter.conf
-echo "logo=/usr/share/backgrounds/logo.png" | sudo tee --append /etc/lightdm/slick-greeter.conf
+echo "[Greeter]" | sudo tee /etc/lightdm/slick-greeter.conf > /dev/null
+echo "background=/usr/share/backgrounds/bg.png" | sudo tee --append /etc/lightdm/slick-greeter.conf > /dev/null
+echo "logo=/usr/share/backgrounds/logo.png" | sudo tee --append /etc/lightdm/slick-greeter.conf > /dev/null
 
 # Reboot the system
 echo "Script completed, reboot required..."
