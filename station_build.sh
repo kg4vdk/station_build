d65fda07cdcbcb8f9565092521cf49d93b9488ac
kg4vdk <mike@kg4vdk.com>
2023-10-03 14:31:21 -0500

    tidying and logging

--- station_build.sh
+++ station_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define the desired background color
+BG_COLOR=#466480
+
 ##############################################
 # Amateur radio software versions to install #
 ##############################################
@@ -14,71 +17,48 @@ WSJTX_VER=2.6.1 # source
 JS8CALL_VER=2.2.0 # appimage
 HAMRS_VER=1.0.6 # appimage
 
-##################################################
-# Define log file and populate basic information #
-##################################################
-LOG_FILE=$HOME/Desktop/station_build.log
+##############
+# BUILD INFO #
+##############
+echo "---------- BUILD INFO ----------" | tee "${LOG_FILE}"
 
+# Define log file and populate basic information
+BUILD_DIR=$(pwd)
+LOG_FILE="${BUILD_DIR}/station_build.log"
 BUILD_DATE=$(date +"%F %R %Z")
 BUILD_VER=$(git show | head -n 1 | awk -F " " '{print $2}')
 OS_VER=$(cat /etc/lsb-release | grep "DISTRIB_DESCRIPTION" | awk -F "=" '{print $2}')
 
-echo "Built: ${BUILD_DATE}" | tee "${LOG_FILE}" # No --append to create a new logfile
+echo "Build Directory: ${BUILD_DIR}" | tee --append "${LOG_FILE}"
+echo "Built: ${BUILD_DATE}" | tee --append "${LOG_FILE}"
 echo "Version: ${BUILD_VER}" | tee --append "${LOG_FILE}"
 echo "OS: ${OS_VER}" | tee --append "${LOG_FILE}"
 
-###################
-# Build directory #
-###################
-# Set the BUILD_DIR variable to the present working directory
-BUILD_DIR=$(pwd)
-
-############################
-# Disclaimers and warnings #
-############################
-echo "##################################################"
-echo -e "
-# BEFORE CONTINUING...
-
-# Please take the opportunity to fully read the contents of the README file, the station_build.sh file,
-# Painstaking effort has been put into documenting the script and the work that it does.
-# It is in your best interest to use this script and the documentation herein to add to your own knowledge.
-# Amateur Radio is a hobby that requires and rewards personal knowledge of how to setup, configure, and operate your equipment.
-# When a computer is part of your station, you should be comfortable rebuilding the software components from scratch in the event of a catastrophic failure.
-# This script is simply an automation of a reproducible build process.
-# You, the user, will have have full control over your system.
-# You are welcome and encouraged to modify this script to suit your own needs/preferences.
-#
-# If you choose to proceed without reading those files...
-# You are on your own! May god have mercy on your soul!
-"
-echo "##################################################"
-
-# Ask the user if they read the files as suggested in the above section
-read -p "Did you read the files as suggested? [Y/N]: " RTFM
-if [ "${RTFM}" == "Y" ] || [ "${RTFM}" == "y" ]; then
-    # If yes, proceed
-    echo "Proceeding..."
-else
-    # If no, exit
-    echo "You are on your own!"
-    echo "Exiting!"
-    exit 0
-fi
+echo "---------- END BUILD INFO ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
 
 ############################################################
 
-###############################
-# Make sure system is updated #
-###############################
+#################
+# SYSTEM UPDATE #
+#################
+echo "---------- SYSTEM UPDATE ----------" | tee --append "${LOG_FILE}"
+
 # Enable source code repositories and update/upgrade the system
 sudo cp --verbose "${BUILD_DIR}/apt/official-source-repositories.list" /etc/apt/sources.list.d/official-source-repositories.list | tee --append "${LOG_FILE}"
 sudo apt update | tee --append "${LOG_FILE}"
 sudo apt upgrade --yes | tee --append "${LOG_FILE}"
 
-######################################
-# Install Virtualbox Guest Additions #
-######################################
+echo "---------- END SYSTEM UPDATE ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+##############################
+# VIRTUALBOX GUEST ADDITIONS #
+##############################
+echo "---------- VIRTUALBOX GUEST ADDITIONS ----------" | tee --append "${LOG_FILE}"
+
 # Define, create, and change into the VBOX_DIR
 VBOX_DIR="${BUILD_DIR}/vbox"
 mkdir --parents --verbose "${VBOX_DIR}" | tee --append "${LOG_FILE}"
@@ -86,7 +66,7 @@ cd "${VBOX_DIR}"
 
 # Determine the version of Virtualbox, so the appropriate Guest Additions ISO is downloaded
 VBOX_VER=$(sudo dmidecode | grep -i vboxver | grep -E -o '[[:digit:]\.]+' | tail -n 1)
-echo "Guest Additions Version: $(echo ${VBOX_VER} || echo 'Not Installed')"
+echo "Virtualbox Version: $(echo ${VBOX_VER} || echo 'Not Installed')" | tee --append "${LOG_FILE}"
 
 # Define the base URL, and download the appropriate version of the Guest Additions ISO
 VBOX_URL_BASE="https://download.virtualbox.org/virtualbox"
@@ -103,23 +83,44 @@ sudo sh /tmp/VBOX_GA_ISO/VBoxLinuxAdditions.run --nox11 | tee --append "${LOG_FI
 sudo umount --verbose /tmp/VBOX_GA_ISO | tee --append "${LOG_FILE}"
 sudo rmdir --verbose /tmp/VBOX_GA_ISO | tee --append "${LOG_FILE}"
 
-#########################################
-# Add user to dialout and vboxsf groups #
-#########################################
+echo "---------- END VIRTUALBOX GUEST ADDITIONS ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+###############
+# USER GROUPS #
+###############
+echo "---------- USER GROUPS ----------" | tee --append "${LOG_FILE}"
+
 # This is required to allow the user to access devices such as serial adapters
 # as well as allowing the user to access Virtualbox Shared Folders
 sudo usermod --append --groups dialout $USER
 sudo usermod --append --groups vboxsf $USER
 echo "User Groups: $(groups)" | tee --append "${LOG_FILE}"
 
-##################################
-# Create directory for appimages #
-##################################
+echo "---------- END USER GROUPS ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+######################
+# APPIMAGE DIRECTORY #
+######################
+echo "---------- APPIMAGE DIRECTORY ----------" | tee --append "${LOG_FILE}"
+
 sudo mkdir --verbose /appimage | tee --append "${LOG_FILE}"
 
-#####################
-# GPS and GPS clock #
-#####################
+echo "---------- END APPIMAGE DIRECTORY ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+#############
+# GPS/CLOCK #
+#############
+echo "---------- GPS/CLOCK ----------" | tee --append "${LOG_FILE}"
+
 # Install gpsd, gpsd-clients, and chrony
 sudo apt install --yes gpsd gpsd-clients chrony | tee --append "${LOG_FILE}"
 
@@ -127,9 +128,16 @@ sudo apt install --yes gpsd gpsd-clients chrony | tee --append "${LOG_FILE}"
 sudo cp --verbose "${BUILD_DIR}/config/gpsd" /etc/default/gpsd | tee --append "${LOG_FILE}"
 sudo cp --verbose "${BUILD_DIR}/config/chrony.conf" /etc/chrony/chrony.conf | tee --append "${LOG_FILE}"
 
-########################
-# Gridsquare Reporting #
-########################
+echo "---------- END GPS/CLOCK ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+##############
+# GRIDSQUARE #
+##############
+echo "---------- GRIDSQUARE ----------" | tee --append "${LOG_FILE}"
+
 # Install ruby and required ruby gems
 sudo apt install --yes ruby | tee --append "${LOG_FILE}"
 sudo gem install gpsd_client maidenhead | tee --append "${LOG_FILE}"
@@ -137,16 +145,30 @@ sudo gem install gpsd_client maidenhead | tee --append "${LOG_FILE}"
 # Copy the ruby script to its location
 sudo cp --verbose "${BUILD_DIR}/bin/gridsquare.rb" /usr/bin/gridsquare.rb | tee --append "${LOG_FILE}"
 
-#############################
-# Crontab for gridsquare.rb #
-#############################
+echo "---------- END GRIDSQUARE ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+###########
+# CRONTAB #
+###########
+echo "---------- CRONTAB ----------" | tee --append "${LOG_FILE}"
+
 # Add a job to the user's crontab to execute the ruby script every 2 minutes
 (crontab -l; cat "${BUILD_DIR}/config/crontab") | crontab -
 echo "Crontab: $(crontab -l)" | tee --append "${LOG_FILE}"
 
-##################
-# Install Hamlib #
-##################
+echo "---------- END CRONTAB ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+##########
+# HAMLIB #
+##########
+echo "---------- HAMLIB ----------" | tee --append "${LOG_FILE}"
+
 # Define, create, and change into the HAMLIB_DIR
 HAMLIB_DIR="${BUILD_DIR}/hamlib"
 mkdir --parents --verbose "${HAMLIB_DIR}" | tee --append "${LOG_FILE}"
@@ -165,9 +187,16 @@ cd "hamlib-${HAMLIB_VER}"
 make | tee --append "${LOG_FILE}"
 sudo make install | tee --append "${LOG_FILE}"
 
-#######################################
-# Install FL Suite (and dependencies) #
-#######################################
+echo "---------- END HAMLIB ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+############
+# FL SUITE #
+############
+echo "---------- FL SUITE ----------" | tee --append "${LOG_FILE}"
+
 # Define, create, and change into the FL_DIR
 FL_DIR="${BUILD_DIR}/fl_suite"
 mkdir --parents --verbose "${FL_DIR}" | tee --append "${LOG_FILE}"
@@ -189,41 +218,58 @@ tar -xvzf "${FL_DIR}/flwrap-${FLWRAP_VER}.tar.gz" | tee --append "${LOG_FILE}"
 tar -xvzf "${FL_DIR}/flamp-${FLAMP_VER}.tar.gz" | tee --append "${LOG_FILE}"
 
 # Install dependencies for FLDIGI
-sudo apt build-dep --yes fldigi
+sudo apt build-dep --yes fldigi | tee --append "${LOG_FILE}"
 
 # Install FLDIGI
+echo "----- FLDIGI -----" | tee --append "${LOG_FILE}"
 cd "${FL_DIR}/fldigi-${FLDIGI_VER}"
 ./configure | tee --append "${LOG_FILE}"
 make | tee --append "${LOG_FILE}"
 sudo make install | tee --append "${LOG_FILE}"
+echo "----- END FLDIGI -----" | tee --append "${LOG_FILE}"
 
 # Install FLRIG
+echo "----- FLRIG -----" | tee --append "${LOG_FILE}"
 cd "${FL_DIR}/flrig-${FLRIG_VER}"
 ./configure | tee --append "${LOG_FILE}"
 make | tee --append "${LOG_FILE}"
 sudo make install | tee --append "${LOG_FILE}"
+echo "----- END FLRIG -----" | tee --append "${LOG_FILE}"
 
 # Install FLMSG
+echo "----- FLMSG -----" | tee --append "${LOG_FILE}"
 cd "${FL_DIR}/flmsg-${FLMSG_VER}"
 ./configure | tee --append "${LOG_FILE}"
 make | tee --append "${LOG_FILE}"
 sudo make install | tee --append "${LOG_FILE}"
+echo "----- END FLMSG -----" | tee --append "${LOG_FILE}"
 
 # Install FLWRAP
+echo "----- FLWRAP -----" | tee --append "${LOG_FILE}"
 cd "${FL_DIR}/flwrap-${FLWRAP_VER}"
 ./configure | tee --append "${LOG_FILE}"
 make | tee --append "${LOG_FILE}"
 sudo make install | tee --append "${LOG_FILE}"
+echo "----- END FLWRAP -----" | tee --append "${LOG_FILE}"
 
 # Install FLAMP
+echo "----- FLAMP -----" | tee --append "${LOG_FILE}"
 cd "${FL_DIR}/flamp-${FLAMP_VER}"
 ./configure | tee --append "${LOG_FILE}"
 make | tee --append "${LOG_FILE}"
 sudo make install | tee --append "${LOG_FILE}"
+echo "----- END FLAMP -----" | tee --append "${LOG_FILE}"
 
-##################
-# Install WSJT-X #
-##################
+echo "---------- END FL SUITE ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+#########
+# WSJTX #
+#########
+echo "---------- WSJTX ----------" | tee --append "${LOG_FILE}"
+
 # Define, create and change into the WSJTX_DIR
 WSJTX_DIR="${BUILD_DIR}/wsjtx"
 mkdir --parents --verbose "${WSJTX_DIR}" | tee --append "${LOG_FILE}"
@@ -246,9 +292,16 @@ cd "${WSJTX_DIR}/wsjtx-${WSJTX_VER}"
 make | tee --append "${LOG_FILE}"
 sudo make install | tee --append "${LOG_FILE}"
 
-###################
-# Install JS8Call #
-###################
+echo "---------- END WSJTX ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+###########
+# JS8CALL #
+###########
+echo "---------- JS8CALL ----------" | tee --append "${LOG_FILE}"
+
 # Define, create, and change into the JS8CALL_DIR
 JS8CALL_DIR="${BUILD_DIR}/js8call"
 mkdir --parents --verbose "${JS8CALL_DIR}" | tee --append "${LOG_FILE}"
@@ -266,9 +319,16 @@ sudo cp --verbose "js8call-${JS8CALL_VER}-Linux-Desktop.x86_64.AppImage" /appima
 sudo cp --verbose "${BUILD_DIR}/desktop/JS8Call.desktop" /usr/share/applications | tee --append "${LOG_FILE}"
 sudo cp --verbose "${BUILD_DIR}/icons/js8call.png" /usr/share/icons | tee --append "${LOG_FILE}"
 
-#################
-# Install HamRS #
-#################
+echo "---------- END JS8CALL ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+#########
+# HAMRS #
+#########
+echo "---------- HAMRS ----------" | tee --append "${LOG_FILE}"
+
 # Define, create, and change into the HAMRS_DIR
 HAMRS_DIR="${BUILD_DIR}/hamrs"
 mkdir --parents --verbose "${HAMRS_DIR}" | tee --append "${LOG_FILE}"
@@ -286,30 +346,33 @@ sudo cp --verbose "hamrs-${HAMRS_VER}-linux-x86_64.AppImage" /appimage | tee --a
 sudo cp --verbose "${BUILD_DIR}/desktop/HamRS.desktop" /usr/share/applications | tee --append "${LOG_FILE}"
 sudo cp --verbose "${BUILD_DIR}/icons/hamrs.png" /usr/share/icons | tee --append "${LOG_FILE}"
 
+echo "---------- END HAMRS ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
 ############################################################
 
-######################################################################
-# Set login screen background and user background to specified color #
-######################################################################
+#####################
+# BACKGROUND IMAGES #
+#####################
+echo "---------- BACKGROUND IMAGES ----------" | tee --append "${LOG_FILE}"
+
 # Define, create, and change into the IMG_DIR
 IMG_DIR="${BUILD_DIR}/img"
 mkdir --parents --verbose "${IMG_DIR}" | tee --append "${LOG_FILE}"
 cd "${IMG_DIR}"
 
-# Install IMAGEMAGICK for background and logo image creation
+# Install IMAGEMAGICK for background image creation
 sudo apt install --yes imagemagick | tee --append "${LOG_FILE}"
 
-# Define the background color and logo text
-BG_COLOR=#466480
 echo "Background Color: ${BG_COLOR}" | tee --append "${LOG_FILE}"
 
 # Create a 96x96 image of the specified color to be used as a background image
 convert -size 96x96 xc:"${BG_COLOR}" "${IMG_DIR}/bg.png" | tee --append "${LOG_FILE}"
 
-# Copy the background and logo images to their respective locations
+# Copy the background image to its location
 sudo cp --verbose "${IMG_DIR}/bg.png" /usr/share/backgrounds/bg.png | tee --append "${LOG_FILE}"
 
-# Create the slick-greeter.conf file to modify the login screen appearance
+# Create the slick-greeter.conf file to set the login screen background image
 echo "[Greeter]" | sudo tee /etc/lightdm/slick-greeter.conf > /dev/null
 echo "background=/usr/share/backgrounds/bg.png" | sudo tee --append /etc/lightdm/slick-greeter.conf > /dev/null
 echo "draw-user-backgrounds=false" | sudo tee --append /etc/lightdm/slick-greeter.conf > /dev/null
@@ -318,24 +381,40 @@ cat /etc/lightdm/slick-greeter.conf | tee --append "${LOG_FILE}"
 # Set the user desktop background image and fallback color as specified
 gsettings set org.cinnamon.desktop.background picture-uri "file:///usr/share/backgrounds/bg.png"
 gsettings set org.cinnamon.desktop.background primary-color "${BG_COLOR}"
-echo "User Background: $(gsettings list-recursively org.cinnamon.desktop.background)"
+echo -e "User Background:\n$(gsettings list-recursively org.cinnamon.desktop.background)"
 
-###################################################
-# Set custom icons for the installed applications #
-###################################################
-# Create the .icons directory in the user's home directory, and copy the custom icons to it.
-mkdir --parents --verbose $HOME/.icons | tee --append "${LOG_FILE}"
-cp --verbose "${BUILD_DIR}/icons/custom/*.png" $HOME/.icons | tee --append "${LOG_FILE}"
+echo "---------- END BACKGROUND IMAGES ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
 
 ############################################################
 
-#####################
-# Reboot the system #
-#####################
+################
+# CUSTOM ICONS #
+################
+echo "---------- CUSTOM ICONS ----------" | tee --append "${LOG_FILE}"
+
+# Create the .icons directory in the user's home directory, and copy the custom icons to it.
+mkdir --parents --verbose $HOME/.icons | tee --append "${LOG_FILE}"
+cp --recursive --verbose "${BUILD_DIR}/icons/custom/*" $HOME/.icons/ | tee --append "${LOG_FILE}"
+
+echo "---------- END CUSTOM ICONS ----------" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
+
+############################################################
+
+#################
+# REBOOT SYSTEM #
+#################
+echo "---------- REBOOT SYSTEM ----------" | tee --append "${LOG_FILE}"
+
 # Inform the user of script completion, and wait for confirmation before rebooting
 echo "Script completed, reboot required..." | tee --append "${LOG_FILE}"
 read -p "Reboot now? [Y/N]: " REBOOT
 if [ "${REBOOT}" == "Y" ] || [ "${REBOOT}" == "y" ]; then
     echo "Rebooting..."
+    echo "---------- END REBOOT SYSTEM ----------" | tee --append "${LOG_FILE}"
+    echo | tee --append "${LOG_FILE}"
     sudo reboot
 fi
+
+############################################################
