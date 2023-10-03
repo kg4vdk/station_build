444b4394db83528d782595683dcebde414803ba4
kg4vdk <mike@kg4vdk.com>
2023-10-03 18:19:23 -0500

    tidying

--- station_build.sh
+++ station_build.sh
@@ -412,32 +412,34 @@ echo | tee --append "${LOG_FILE}"
 IMG_DIR="${BUILD_DIR}/img"
 mkdir --parents --verbose "${IMG_DIR}" |& tee --append "${LOG_FILE}"
 cd "${IMG_DIR}"
+echo | tee --append "${LOG_FILE}"
 
 # Install IMAGEMAGICK for background image creation
 sudo apt install --yes imagemagick |& tee --append "${LOG_FILE}"
 
-echo | tee --append "${LOG_FILE}"
 echo "Background Color: ${BG_COLOR}" | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
 
 # Create a 96x96 image of the specified color to be used as a background image
 convert -size 96x96 xc:"${BG_COLOR}" "${IMG_DIR}/bg.png" |& tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
 
 # Copy the background image to its location
 sudo cp --verbose "${IMG_DIR}/bg.png" /usr/share/backgrounds/bg.png |& tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
 
 # Create the slick-greeter.conf file to set the login screen background image
 echo "[Greeter]" | sudo tee /etc/lightdm/slick-greeter.conf > /dev/null
 echo "background=/usr/share/backgrounds/bg.png" | sudo tee --append /etc/lightdm/slick-greeter.conf > /dev/null
 echo "draw-user-backgrounds=false" | sudo tee --append /etc/lightdm/slick-greeter.conf > /dev/null
 
-echo | tee --append "${LOG_FILE}"
 echo "Contents of /etc/lightdm/slick-greeter.conf:" | tee --append "${LOG_FILE}"
 cat /etc/lightdm/slick-greeter.conf | tee --append "${LOG_FILE}"
+echo | tee --append "${LOG_FILE}"
 
 # Set the user desktop background image and fallback color as specified
 gsettings set org.cinnamon.desktop.background picture-uri "file:///usr/share/backgrounds/bg.png"
 gsettings set org.cinnamon.desktop.background primary-color "${BG_COLOR}"
-echo | tee --append "${LOG_FILE}"
 echo -e "User Background Settings:\n$(gsettings list-recursively org.cinnamon.desktop.background)"
 echo | tee --append "${LOG_FILE}"
 
