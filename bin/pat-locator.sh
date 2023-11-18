315bd65953ad88a10b6cbdb0c4e93dee48cc17c6
kg4vdk <mike@kg4vdk.com>
2023-11-17 19:44:02 -0600

    update

--- bin/pat-locator.sh
+++ bin/pat-locator.sh
@@ -2,7 +2,7 @@
 
 # Check if there is a valid GPS fix, and update Pat config file if needed.
 if [ -f /tmp/grid.log ]; then
-    GRID=$(cat /tmp/grid.log)
+    GRID=$(cat /tmp/grid.log | cut -c -6)
     # Set Pat locator if available from GPS
     jq --arg GRID "$GRID" '.locator = $GRID' "${HOME}/.config/pat/config.json" > /tmp/config.json
     mv /tmp/config.json "${HOME}/.config/pat/config.json"
